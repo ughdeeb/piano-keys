75cf6be07744b30abc01150877ffd930bb2248d5
ughdeeb <adeebsyed725@gmail.com>
2021-01-22 21:19:42 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# # Codecademy Project: Piano Project
+
+## Project Title: Piano Project
+
+### Project Objective: Create a piano player with DOM events in JavaScript!
+
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Setup](#setup)
+
+## General info
+
+You’re a web developer who has been hired by a music education company. This client wants you to create an interactive game to help their beginner-level piano students study. Create a piano player with DOM events in JavaScript!
+
+## Technologies
+
+Technologies used in the creation of this project:
+
+- HTML
+- CSS
+- JS ES6
+- CLI
+- Git and Github (with VS Code)
--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -50,16 +50,56 @@ let startOver = document.getElementById("fourth-next-line");
 // This variable stores the '-END' lyric element
 let lastLyric = document.getElementById("column-optional");
 
-// These statements are "hiding" all the progress buttons, but the first one
+// These statements are "hiding" al`l the progress buttons, but the first one
 nextTwo.hidden = true;
 nextThree.hidden = true;
 startOver.hidden = true;
 
 // Write anonymous event handler property and function for the first progress button
+nextOne.onclick = function () {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+};
 
 // Write anonymous event handler property and function for the second progress button
+nextTwo.onclick = function () {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
 
+  document.getElementById("word-five").innerHTML = "DEAR";
+  document.getElementById("word-six").innerHTML = "FRI-";
+
+  document.getElementById("letter-note-three").innerHTML = "G";
+  document.getElementById("letter-note-four").innerHTML = "E";
+  document.getElementById("letter-note-five").innerHTML = "C";
+  document.getElementById("letter-note-six").innerHTML = "B";
+
+  lastLyric.style.display = "inline-block";
+};
 // Write anonymous event handler property and function for the third progress button
+nextThree.onclick = function () {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+
+  document.getElementById("word-one").innerHTML = "HAP-";
+  document.getElementById("word-two").innerHTML = "PY";
+  document.getElementById("word-three").innerHTML = "BIRTH";
+  document.getElementById("word-four").innerHTML = "DAY";
+  document.getElementById("word-five").innerHTML = "TO";
+  document.getElementById("word-six").innerHTML = "YOU";
+
+  document.getElementById("letter-note-one").innerHTML = "F";
+  document.getElementById("letter-note-two").innerHTML = "F";
+  document.getElementById("letter-note-three").innerHTML = "E";
+  document.getElementById("letter-note-four").innerHTML = "C";
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+
+  lastLyric.style.display = "none";
+};
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function () {
